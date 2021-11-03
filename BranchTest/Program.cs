ba617f7fc0163e66364bcf6a878a1613d128d2ef
Calyn Greene <calyngreene@rocketmortgage.com>
2021-11-03 14:58:54 -0400

    commiting again with the same line

--- BranchTest/Program.cs
+++ BranchTest/Program.cs
@@ -6,7 +6,10 @@ namespace BranchTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I have a fat cat");
+            Console.WriteLine("His name is Stone Capone Greene");
+            Console.WriteLine("I wish I had a dog");
+            Console.WriteLine("But I have a cat");
         }
     }
 }
